75bd4fcc044386d391e04db06ab002c758a9053d
Daniel Kurowski <daniel.kurowski@grifart.cz>
2020-05-29 14:29:53 +0200

    Interaction: for..in to forEach

--- src/handleInteraction.ts
+++ src/handleInteraction.ts
@@ -3,14 +3,8 @@ import {scrollTo} from './scrollTo';
 export function handleInteraction(): void
 {
 	document.addEventListener('DOMContentLoaded', () => {
-		const items = document.querySelectorAll('a[href^="#"]');
-
-		for (const i in items) {
-			if ( ! (items as object).hasOwnProperty(i)) {
-				continue;
-			}
-
-			items[i].addEventListener('click', (e) => {
+		document.querySelectorAll('a[href^="#"]').forEach((item) => {
+			item.addEventListener('click', (e) => {
 				const element = e.currentTarget as HTMLAnchorElement;
 				if ( ! element.hash) {
 					return;
@@ -19,6 +13,6 @@ export function handleInteraction(): void
 				e.preventDefault();
 				scrollTo(element.hash);
 			});
-		}
+		});
 	});
 }
