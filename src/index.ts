5f64a3278bfceb56153e22dc204dc8e2e2144d18
Daniel Kurowski <daniel.kurowski@grifart.cz>
2020-06-08 09:36:57 +0200

    Exposed scrollTo*() methods through API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import * as Velocity from 'velocity-animate';
 import setupVelocity from './easing/setupVelocity';
 import {initializeOnLoadScroll} from './handlers/loadScroll/initializeOnLoadScroll';
 import {initializeOnLinkClickScroll} from './handlers/linkClickScroll/initializeOnLinkClickScroll';
+import {scrollToElement} from './scrollers/scrollToElement';
+import {scrollToOffset} from './scrollers/scrollToOffset';
+import {scrollToTarget} from './scrollers/scrollToTarget';
+import {HashTarget} from './HashTarget';
 
 export interface SmoothScrollOptions {
 	readonly scrollOnLoad?: boolean;
@@ -9,7 +13,7 @@ export interface SmoothScrollOptions {
 }
 
 /**
- * Wrapped into class for intuitive API use – `SmoothScroll.enable()`
+ * Wrapped into class for intuitive API use – `SmoothScroll.something()`
  */
 class SmoothScroll
 {
@@ -24,6 +28,21 @@ class SmoothScroll
 		if ( ! (options && options.scrollOnLinkClick === false)) {
 			initializeOnLinkClickScroll();
 		}
+	}
+
+	public static scrollToElement(element: HTMLElement, onScrollFinishedCallback?: () => void): void
+	{
+		scrollToElement(element, onScrollFinishedCallback);
+	}
+
+	public static scrollToOffset(topOffset: number, onScrollFinishedCallback?: () => void): void
+	{
+		scrollToOffset(topOffset, onScrollFinishedCallback);
+	}
+
+	public static scrollToTarget(target: HashTarget, onScrollFinishedCallback?: () => void): void
+	{
+		scrollToTarget(target, onScrollFinishedCallback);
 	}
 }
 
