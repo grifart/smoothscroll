b9aa4df007d1c19cbe15ae02c1a631262c19717b
Daniel Kurowski <daniel.kurowski@grifart.cz>
2021-04-30 09:42:58 +0200

    yarn upgrade --latest

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,30 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('velocity-animate')) :
 	typeof define === 'function' && define.amd ? define(['exports', 'velocity-animate'], factory) :
-	(factory((global.SmoothScroll = {}),global.Velocity));
-}(this, (function (exports,Velocity) { 'use strict';
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.SmoothScroll = {}, global.Velocity));
+}(this, (function (exports, Velocity) { 'use strict';
+
+	function _interopNamespace(e) {
+		if (e && e.__esModule) return e;
+		var n = Object.create(null);
+		if (e) {
+			Object.keys(e).forEach(function (k) {
+				if (k !== 'default') {
+					var d = Object.getOwnPropertyDescriptor(e, k);
+					Object.defineProperty(n, k, d.get ? d : {
+						enumerable: true,
+						get: function () {
+							return e[k];
+						}
+					});
+				}
+			});
+		}
+		n['default'] = e;
+		return Object.freeze(n);
+	}
+
+	var Velocity__namespace = /*#__PURE__*/_interopNamespace(Velocity);
 
 	var EASE_IN_SKIP_OUT_EASING = 'ease-in-skip-out'; // e.g. (5, 5, 10, 500, 1000) => 500
 	// e.g. (5, 0, 10, 500, 1000) => 750
@@ -115,7 +137,7 @@
 	}());
 
 	function scrollToElement(element, onScrollFinishedCallback) {
-	    Velocity.animate(element, 'scroll', {
+	    Velocity__namespace.animate(element, 'scroll', {
 	        duration: 1200,
 	        easing: EASE_IN_SKIP_OUT_EASING,
 	        complete: function () { return onScrollFinishedCallback !== undefined && onScrollFinishedCallback(); },
@@ -123,29 +145,31 @@
 	}
 
 	/*! *****************************************************************************
-	Copyright (c) Microsoft Corporation. All rights reserved.
-	Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-	this file except in compliance with the License. You may obtain a copy of the
-	License at http://www.apache.org/licenses/LICENSE-2.0
+	Copyright (c) Microsoft Corporation.
 
-	THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-	KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-	WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-	MERCHANTABLITY OR NON-INFRINGEMENT.
+	Permission to use, copy, modify, and/or distribute this software for any
+	purpose with or without fee is hereby granted.
 
-	See the Apache Version 2.0 License for specific language governing permissions
-	and limitations under the License.
+	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+	REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+	INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+	LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+	OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+	PERFORMANCE OF THIS SOFTWARE.
 	***************************************************************************** */
 	/* global Reflect, Promise */
 
 	var extendStatics = function(d, b) {
 	    extendStatics = Object.setPrototypeOf ||
 	        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-	        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+	        function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
 	    return extendStatics(d, b);
 	};
 
 	function __extends(d, b) {
+	    if (typeof b !== "function" && b !== null)
+	        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
 	    extendStatics(d, b);
 	    function __() { this.constructor = d; }
 	    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
@@ -213,7 +237,7 @@
 	}
 
 	function scrollToOffset(topOffset, onScrollFinishedCallback) {
-	    Velocity.animate(document.documentElement, 'scroll', {
+	    Velocity__namespace.animate(document.documentElement, 'scroll', {
 	        duration: 1200,
 	        offset: topOffset,
 	        easing: EASE_IN_SKIP_OUT_EASING,
@@ -221,7 +245,7 @@
 	    });
 	}
 
-	bindEasingToVelocity(Velocity);
+	bindEasingToVelocity(Velocity__namespace);
 	function handleOnLoadScroll() {
 	    initializeOnLoadScroll();
 	}
@@ -230,8 +254,8 @@
 	}
 
 	exports.HashTarget = HashTarget;
-	exports.handleOnLoadScroll = handleOnLoadScroll;
 	exports.handleOnLinkClickScroll = handleOnLinkClickScroll;
+	exports.handleOnLoadScroll = handleOnLoadScroll;
 	exports.scrollToElement = scrollToElement;
 	exports.scrollToOffset = scrollToOffset;
 	exports.scrollToTarget = scrollToTarget;
@@ -239,3 +263,4 @@
 	Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
+//# sourceMappingURL=index.js.map
