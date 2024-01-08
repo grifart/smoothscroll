f866b64a343c304229d9cd29d8c181bddbe0202a
Daniel Kurowski <daniel.kurowski@grifart.cz>
2024-01-08 14:49:26 +0100

    upgrade build stack

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
-const pkg = require('./package.json');
+import pkg from './package.json' with {type: 'json'};
 
 export default {
 	input: 'src/index.ts',
