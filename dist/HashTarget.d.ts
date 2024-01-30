ea2d4c62a78809a5d7850c90c7410a4bc53883e0
Daniel Kurowski <daniel.kurowski@grifart.cz>
2024-01-30 11:14:34 +0100

    remove obsolete dist files

--- dist/HashTarget.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class HashTarget {
-    private readonly value;
-    private constructor();
-    static fromString(value: string): HashTarget;
-    getHash(): string;
-    getRefElement(document: HTMLDocument): HTMLElement;
-}
