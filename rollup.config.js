fc74feca11751692b5d372590866f9796bcaaa10
Daniel Kurowski <daniel.kurowski@grifart.cz>
2024-01-30 10:41:47 +0100

    build only esm version

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,21 @@
 import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json' with {type: 'json'};
 
 export default {
 	input: 'src/index.ts',
-	output: [
-		{ file: pkg.main, format: 'umd', name: 'SmoothScroll', globals: { 'velocity-animate': 'Velocity' }, sourcemap: true },
-		{ file: pkg.module, format: 'esm', sourcemap: true },
-	],
+	output: {
+		file: pkg.main,
+		format: 'esm',
+		sourcemap: true,
+	},
 	external: [
 		/@babel\/runtime/, // see https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
 		...Object.keys(pkg.dependencies || {}),
 		...Object.keys(pkg.peerDependencies || {}),
 	],
 	plugins: [
-		resolve(),
 		typescript(),
 		babel({
 			babelHelpers: 'runtime',
